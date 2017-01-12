81911cce618ba867d89395fe7d8ab5045579c661
Kevin McCann <kevinmccann@zipcoders-MacBook-Pro-5.local>
2017-01-12 16:46:40 -0500

    2Large2Small

---
+++ TooLargeTooSmall/src/main/java/mccann/kevin/toolargetoosmall/RandomNumber.java
@@ -0,0 +1,13 @@
+package mccann.kevin.toolargetoosmall;
+
+/**
+ * Created by kevinmccann on 1/12/17.
+ */
+public class RandomNumber {
+    private int number = (int) (Math.random()*100);
+
+    public int getRand(){
+        return this.number;
+    }
+
+}
