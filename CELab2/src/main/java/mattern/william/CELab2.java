3bd8ea680aaa5ae0f420f8663f63e10394b3e7eb
William Mattern <williammattern@zipcoders-MacBook-Pro-3.local>
2017-01-12 20:22:06 -0500

    Completed CELab2

--- CELab2/src/main/java/mattern/william/CELab2.java
+++ CELab2/src/main/java/mattern/william/CELab2.java
@@ -4,4 +4,16 @@ package mattern.william;
  * Created by williammattern on 1/12/17.
  */
 public class CELab2 {
+
+    public static void main(String[] args) {
+        Prompter prompter = new Prompter();
+        String userName = prompter.getUserName();
+        NameCheck nameCheck = new NameCheck();
+        Boolean result = nameCheck.checkThisNameOut(userName);
+        if (result) {
+            System.out.println("Hello " + userName);
+        } else {
+            System.out.println("Access Denied");
+        }
+    }
 }
--- CELab2/src/test/java/mattern/william/CELab2Test.java
+++ CELab2/src/test/java/mattern/william/CELab2Test.java
@@ -9,13 +9,8 @@ import static org.junit.Assert.*;
  */
 public class CELab2Test {
 
-    public static void main(String[] args) {
-        Prompter prompter = new Prompter();
-        String userName = prompter.getUserName();
-        NameCheck nameCheck = new NameCheck();
-        Boolean result = nameCheck.checkThisNameOut(userName);
-        System.out.println(result);
 
-    }
+
+
 
 }
