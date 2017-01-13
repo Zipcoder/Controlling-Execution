b433cbf6bf67c60064ba9ec669a06a2232907554
William Mattern <williammattern@zipcoders-MacBook-Pro-3.local>
2017-01-12 19:29:02 -0500

    Added UML

--- LabCE1/src/main/java/mattern/william/Prompter.java
+++ LabCE1/src/main/java/mattern/william/Prompter.java
@@ -6,8 +6,6 @@ import java.util.Scanner;
  * Created by williammattern on 1/12/17.
  */
 public class Prompter {
-    int userNumber;
-    int userNumberAddedTogether = 0;
     Scanner scanner = new Scanner(System.in);
 
     public int prompt(){
