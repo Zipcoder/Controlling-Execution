062fefb1c3543629d64e53e9ff57fb3a059fb414
William Mattern <williammattern@zipcoders-MacBook-Pro-3.local>
2017-01-12 20:38:22 -0500

    initialized CELab3

---
+++ CELab3/src/main/java/mattern/william/CELab3.java
@@ -0,0 +1,11 @@
+package mattern.william;
+
+/**
+ * Created by williammattern on 1/12/17.
+ */
+public class CELab3 {
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ CELab3/src/test/java/mattern/william/CELab3Test.java
@@ -0,0 +1,10 @@
+package mattern.william;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by williammattern on 1/12/17.
+ */
+public class CELab3Test {
+
+}
